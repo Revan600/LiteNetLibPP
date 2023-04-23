e6f9fcea9da0cc4e308359aeb7df3ea69a4f052a
Revan <contact@revan.work>
2023-04-24 02:08:32 +0400

    Made net_event_listener just virtual instead of abstract

--- include/lnl/net_event_listener.h
+++ include/lnl/net_event_listener.h
@@ -9,23 +9,23 @@
 namespace lnl {
     class net_event_listener {
     public:
-        virtual void on_peer_connected(std::shared_ptr<net_peer>& peer) = 0;
+        virtual void on_peer_connected(std::shared_ptr<net_peer>& peer) {};
 
-        virtual void on_peer_disconnected(std::shared_ptr<net_peer>& peer, disconnect_info& disconnectInfo) = 0;
+        virtual void on_peer_disconnected(std::shared_ptr<net_peer>& peer, disconnect_info& disconnectInfo) {};
 
         virtual void on_network_error(const net_address& endpoint, uint32_t socketErrorCode,
-                                      const std::string& message) = 0;
+                                      const std::string& message) {};
 
         virtual void on_network_receive(std::shared_ptr<net_peer>& peer, net_data_reader& reader,
-                                        uint8_t channelNumber, DELIVERY_METHOD deliveryMethod) = 0;
+                                        uint8_t channelNumber, DELIVERY_METHOD deliveryMethod) {};
 
         virtual void on_network_receive_unconnected(const net_address& endpoint, net_data_reader& reader,
-                                                    UNCONNECTED_MESSAGE_TYPE messageType) = 0;
+                                                    UNCONNECTED_MESSAGE_TYPE messageType) {};
 
-        virtual void on_network_latency_update(std::shared_ptr<net_peer>& peer, int latency) = 0;
+        virtual void on_network_latency_update(std::shared_ptr<net_peer>& peer, int latency) {};
 
-        virtual void on_connection_request(std::shared_ptr<net_connection_request>& request) = 0;
+        virtual void on_connection_request(std::shared_ptr<net_connection_request>& request) {};
 
-        virtual void on_message_delivered(std::shared_ptr<net_peer>& peer, void* userData) = 0;
+        virtual void on_message_delivered(std::shared_ptr<net_peer>& peer, void* userData) {};
     };
 }
